13497ef26216a52d6a1eebadeb8cb15439af4461
Kamari93 <kmoore971@gmail.com>
2026-01-09 09:23:47 -0600

    D: added player.py class and functionality

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 def main():
     pygame.init()
     # get a new instance of GUI window:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -17,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         delta_time = clock.tick(60)
         convert_to_secs = delta_time / 1000
