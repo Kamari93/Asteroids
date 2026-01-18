3a1c1633b7485f6b27fa4724917514c60c20ea73
Kamari93 <kmoore971@gmail.com>
2026-01-18 15:58:06 -0600

    N: updated split logic so that smaller asteroids properly get destroyed, resolved bug

--- asteroid.py
+++ asteroid.py
@@ -16,16 +16,16 @@ class Asteroid(CircleShape):
         self.position += self.velocity * dt
 
     def split(self):
-        if self.radius <= ASTEROID_MIN_RADIUS:
-            return
-        log_event("asteroid_split")
-        # Create two smaller asteroids
-        random_angle = random.uniform(20, 50)
-        first_new_asteroid_movement = self.velocity.rotate(random_angle)
-        second_new_asteroid_movement = self.velocity.rotate(-random_angle)
-        new_radisus_smaller_asteroid = self.radius - ASTEROID_MIN_RADIUS
-        first_asteroid = Asteroid(self.position.x, self.position.y, new_radisus_smaller_asteroid)
-        second_asteroid = Asteroid(self.position.x, self.position.y, new_radisus_smaller_asteroid)
-        first_asteroid.velocity = first_new_asteroid_movement * 1.2
-        second_asteroid.velocity = second_new_asteroid_movement * 1.2
+        if self.radius > ASTEROID_MIN_RADIUS:
+        
+            log_event("asteroid_split")
+            # Create two smaller asteroids
+            random_angle = random.uniform(20, 50)
+            first_new_asteroid_movement = self.velocity.rotate(random_angle)
+            second_new_asteroid_movement = self.velocity.rotate(-random_angle)
+            new_radisus_smaller_asteroid = self.radius - ASTEROID_MIN_RADIUS
+            first_asteroid = Asteroid(self.position.x, self.position.y, new_radisus_smaller_asteroid)
+            second_asteroid = Asteroid(self.position.x, self.position.y, new_radisus_smaller_asteroid)
+            first_asteroid.velocity = first_new_asteroid_movement * 1.2
+            second_asteroid.velocity = second_new_asteroid_movement * 1.2
         super().kill()
